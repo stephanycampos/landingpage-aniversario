3c0745ec1553945ddb3dd81954acd9399d54f36c
Stephany <stephanyenne@gmail.com>
2025-07-30 20:59:11 -0300

    adicionada build

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,29 @@
-console.log("ola mundo");
+AOS.init();
+
+const dataDoEvento = new Date("May 03, 2026 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function () {
+  const agora = new Date();
+  const timeStampAtual = agora.getTime();
+
+  const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+  const diaEmMs = 1000 * 60 * 60 * 24;
+  const horaEmMs = 1000 * 60 * 60;
+  const minutoEmMs = 1000 * 60;
+
+  const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+  const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+  const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutoEmMs);
+  const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+  console.log(diasAteOEvento);
+  console.log(horasAteOEvento);
+  console.log(minutosAteOEvento);
+  console.log(segundosAteOEvento);
+
+  document.getElementById('contador').innerHTML = 
+    `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+}, 1000);
